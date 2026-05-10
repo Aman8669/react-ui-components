7fb23075d51bc1dfe103c3e5b21f9c5779cce4e6
Aman Naikwade <amannaikwade09@gmail.com>
2026-05-10 17:12:18 +0530

    Created responsive navbar using React

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// Navbar.jsx
+import React from 'react'
+import '../Style/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href=""><h1>Branding**</h1></a>
+      </div>
+
+      <div className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
